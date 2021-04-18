230152e8b905918080d1fc44b869fa7273bb4649
Aaron Williams <aaronrw4@yahoo.com>
2021-04-18 00:25:39 -0700

    Updates

---
+++ precache-manifest.a7a835a784466b64f88bd4b670313ddf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb327614de0800451e666c91ae5de0b",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/index.html"
+  },
+  {
+    "revision": "069bdb9b0c740b92c312",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/css/main.5af0d07d.chunk.css"
+  },
+  {
+    "revision": "1872a979954ed3c7775e",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/js/2.cebd69c7.chunk.js"
+  },
+  {
+    "revision": "069bdb9b0c740b92c312",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/js/main.d8cbff33.chunk.js"
+  },
+  {
+    "revision": "c6e3213f1b3a108e1c80",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/js/runtime~main.1487400e.js"
+  }
+]);
